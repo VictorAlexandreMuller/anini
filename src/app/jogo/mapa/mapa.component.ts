ffad3dd46534919ca4be3b658f553bb3e948d1ca
FrokenNotebook <victor.alexandre.mu@gmail.com>
2025-06-07 12:30:45 -0300

    fases ajustadas

--- src/app/jogo/mapa/mapa.component.ts
+++ src/app/jogo/mapa/mapa.component.ts
@@ -18,7 +18,7 @@ export class MapaComponent implements AfterViewInit {
   faseAtual = 0;
 
   @ViewChildren(
-    'fase0, fase1, fase2, fase3, fase4, fase5, fase6, fase7, fase8, fase9, fase10'
+    'fase0, fase1, fase2, fase3, fase4, fase5, fase6, fase7, fase8, fase9, fase10, fase11, fase12, fase13, fase14, fase15, fase16, fase17, fase18, fase19, fase20, fase21, fase22, fase23, fase24, fase25, fase26, fase27, fase28, fase29, fase30'
   )
   fasesRef!: QueryList<ElementRef>;
 
@@ -31,14 +31,14 @@ export class MapaComponent implements AfterViewInit {
   }
 
   mostrarFase(i: number): boolean {
-    if (i <= 4) return true;
-    if (i <= 9 && this.faseAtual >= 4) return true;
-    if (i === 10 && this.faseAtual >= 9) return true;
+    if (i <= 10) return true;
+    if (i <= 20 && this.faseAtual >= 10) return true;
+    if (i <= 30 && this.faseAtual >= 20) return true;
     return false;
   }
 
   avançarFase() {
-    if (this.faseAtual < 10) {
+    if (this.faseAtual < 30) {
       this.faseAtual++;
       this.atualizarPosicao();
     }
