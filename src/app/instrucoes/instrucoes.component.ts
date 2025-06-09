e010e2a771d80fb40ae18b201de60b5fa3d01ff9
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2025-06-08 23:59:15 -0300

    instrucoes mobile

--- src/app/instrucoes/instrucoes.component.ts
+++ src/app/instrucoes/instrucoes.component.ts
@@ -1,17 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-instrucoes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbCarouselModule],
   templateUrl: './instrucoes.component.html',
-  styleUrls: ['./instrucoes.component.scss']
+  styleUrls: ['./instrucoes.component.scss'],
 })
 export class InstrucoesComponent {
-    @Output() fechar = new EventEmitter<void>();
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  @Output() fechar = new EventEmitter<void>();
 
-  irParaJogo() {
-    // implemente a navegação ou comunicação com app.component
+  slides = [
+    '🔍 <br> Leia as pistas de cada fase na parte superior da tela.',
+    '💎 <br> Procure pelos tesouros escondidos.',
+    '🔖 <br> Cada tesouro terá um código, utilize deste código para avançar pelas trilhas e conquistar todos os tesouros escondidos.',
+    '🎉 <br> Boa Sorte!',
+  ];
+  currentSlide = 0;
+
+  voltarSlide() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  avancarSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
   }
 }
